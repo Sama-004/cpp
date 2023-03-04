047ddc4ed757f1ce3dfd5b53bda1b61c2106d0f8
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-03-04 19:36:10 +0530

    init

---
+++ 2in.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::end;
+
+int main()
+{
+    cout << "enter two number x and y seperated by a space :  " ;
+    int x{};
+    int y{};
+    cin >> x >> y ;
+    cout << "The numbers you entered are " << x << " and " << y << '\n' ;
+    return 0;
+
+
+
+
+
+}
